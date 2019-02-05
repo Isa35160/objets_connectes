7d75ea91e54d04ffa95fb651f3b681a8b89a3d75
Altémir <a.barrier92@gmail.com>
2019-02-05 15:44:31 +0100

    ajout weather

--- weather.py
+++ weather.py
@@ -20,5 +20,5 @@ print(w)                      # <Weather - reference time=2013-12-18 09:20,
 w.get_wind()                  # {'speed': 4.6, 'deg': 330}
 w.get_humidity()              # 87
 w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-
+print(w)
 observation_list = owm.weather_around_coords(48.117266, -1.6777926)
