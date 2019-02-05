7d8d2879393b7745379025e63a40a4026bee12b9
Altémir <a.barrier92@gmail.com>
2019-02-05 15:47:47 +0100

    ajout weather

--- weather.py
+++ weather.py
@@ -12,13 +12,14 @@ owm = pyowm.OWM('58750219130143b7987aa6c46cae85f8')  # You MUST provide a valid 
 
 # Search for current weather in London (Great Britain)
 observation = owm.weather_at_place('Rennes, Fr')
-w = observation.get_temperature('celsius')
+w = observation.get_weather()
 print(w)                      # <Weather - reference time=2013-12-18 09:20,
                               # status=Clouds>
 
 # Weather details
-w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-w.get_humidity()              # 87
-w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-
+m = observation.get_weather()
+m.get_wind()                  # {'speed': 4.6, 'deg': 330}
+m.get_humidity()              # 87
+m.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+print(m.get_temperature('celcius'))
 observation_list = owm.weather_around_coords(48.117266, -1.6777926)
