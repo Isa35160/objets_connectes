8caf56ca8eeab286c1d2d323bdb2aa47fc45a803
Altémir <a.barrier92@gmail.com>
2019-02-05 14:00:31 +0100

    modif app.js

--- lightSensor.py
+++ lightSensor.py
@@ -12,14 +12,15 @@ class LightSensor:
     GPIO.setwarnings(False)
 
     def read_light(self, socketIo):
-        lightCount = 0  # intitialisation de la variable de lumière
-        GPIO.setup(self.broche, GPIO.OUT)
-        GPIO.output(self.broche, GPIO.LOW)
-        time.sleep(0.1)  # on draine la charge du condensateur
-        GPIO.setup(self.broche, GPIO.IN)
-        # Tant que la broche lit ‘off’ on incrémente notre variable
-        while GPIO.input(self.broche) == GPIO.LOW:
-            lightCount += 1
-            socketIo.emit('LightLive', lightCount, Broadcast=True)
+        while True:
+            lightCount = 0  # intitialisation de la variable de lumière
+            GPIO.setup(self.broche, GPIO.OUT)
+            GPIO.output(self.broche, GPIO.LOW)
+            time.sleep(0.1)  # on draine la charge du condensateur
+            GPIO.setup(self.broche, GPIO.IN)
+            # Tant que la broche lit ‘off’ on incrémente notre variable
+            while GPIO.input(self.broche) == GPIO.LOW:
+                lightCount += 1
+                socketIo.emit('LightLive', lightCount, Broadcast=True)
 
 
